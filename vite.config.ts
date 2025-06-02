cecae0532a68b447f356f75d518f99c94675dd84
sachin1kumar <sachinkmr375@gmail.com>
2025-06-02 17:19:27 +0530

    My bolt commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rsample-bolt-diy/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
